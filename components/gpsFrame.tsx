d77d6bc6300789825cd40bdb4a30e5ab60de0464
mdsp <mdsp.personal@gmail.com>
2025-03-26 22:12:09 -0300

    fix: iframe error deploy

--- components/gpsFrame.tsx
+++ components/gpsFrame.tsx
@@ -4,7 +4,7 @@ export default function GpsFrame() {
   return (
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3981.244282800648!2d-47.50135272502595!3d-3.756916696216977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zM8KwNDUnMjQuOSJTIDQ3wrAyOSc1NS42Ilc!5e0!3m2!1sen!2sbr!4v1743035366807!5m2!1sen!2sbr"
-      
+      title="HeadquartersLocation"
       className="w-[56vw] h-[44vw] md:w-[40vw] md:h-[30vw] lg:w-[28vw] lg:h-[20vw] rounded-lg"
       allowFullScreen
       loading="lazy" ></iframe>
